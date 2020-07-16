ac79ae2c170df119a16544fa05e8c162b0d41833
patrick <patrick204nqh@gmail.com>
2020-07-16 21:10:30 +0700

    completing & removing todos

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 // Selectors
 document.querySelector('form').addEventListener('submit', handleSubmitForm);
+document
+  .querySelector('ul')
+  .addEventListener('click', handleClickDeleteOrCheck);
+document.getElementById('clearAll').addEventListener('click', handleClearAll);
 
 // Event Handlers
 function handleSubmitForm(e) {
@@ -9,6 +13,19 @@ function handleSubmitForm(e) {
     addTodo(input);
   }
   input.value = '';
+}
+
+function handleClickDeleteOrCheck(e) {
+  if (e.target.name == 'checkButton') {
+    checkTodo(e);
+  }
+  if (e.target.name == 'deleteButton') {
+    deleteTodo(e);
+  }
+}
+
+function handleClearAll(e) {
+  document.querySelector('ul').innerHTML = '';
 }
 
 // Helpers
@@ -24,4 +41,23 @@ function addTodo(todo) {
 
   li.classList.add('todo-list-item');
   ul.appendChild(li);
+}
+
+function checkTodo(e) {
+  let item = e.target.parentNode;
+  if (item.style.textDecoration == 'line-through') {
+    item.style.textDecoration = 'none';
+  } else {
+    item.style.textDecoration = 'line-through';
+  }
+}
+
+function deleteTodo(e) {
+  let item = e.target.parentNode;
+
+  item.addEventListener('transitionend', function () {
+    item.remove();
+  });
+
+  item.classList.add('todo-list-item-fall');
 }
